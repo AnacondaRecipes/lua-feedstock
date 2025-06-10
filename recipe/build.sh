7f7d5aa26405a3b5b5b99cb5fd2cd18de009a8df
Tomasz Beneś <tbenes@anaconda.com>
2025-06-10 12:39:23 +0200

    activation scripts

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,18 @@
+CHANGE="deactivate"
+mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+
+CHANGE="activate"
+mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+sed -i.bak "s|@PKG_VERSION@|${PKG_VERSION}|g" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+sed -i.bak "s|@SHLIB_EXT@|${SHLIB_EXT}|g" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+
 if [ `uname` == Darwin ]; then
-	make macosx INSTALL_TOP=$PREFIX MYCFLAGS="-fPIC -I$PREFIX/include -L$PREFIX/lib -DLUA_USER_DEFAULT_PATH='\"$PREFIX/\"'" MYLDFLAGS="-L$PREFIX/lib -Wl,-rpath,$PREFIX/lib"
+	make macosx INSTALL_TOP=$PREFIX MYCFLAGS="-fPIC -I$PREFIX/include" MYLDFLAGS="-Wl,-rpath,$PREFIX/lib"
 	make macosx test
 elif [ `uname` == Linux ]; then
-	make linux INSTALL_TOP=$PREFIX MYCFLAGS="-fPIC -I$PREFIX/include -L$PREFIX/lib -DLUA_USE_LINUX -DLUA_USER_DEFAULT_PATH='\"$PREFIX/\"'" MYLDFLAGS="-L$PREFIX/lib -Wl,-rpath=$PREFIX/lib"
+	make linux INSTALL_TOP=$PREFIX MYCFLAGS="-fPIC -I$PREFIX/include -DLUA_USE_LINUX" MYLDFLAGS="-Wl,-rpath=$PREFIX/lib"
 	make linux-readline test
 fi
 make install INSTALL_TOP=$PREFIX
